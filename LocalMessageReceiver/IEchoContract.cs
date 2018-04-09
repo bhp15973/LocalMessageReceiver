9e12e9990a97fb3df06abf7c1685f1a5306d6bbe
PGS-SOFT\bmaciejewski <bmaciejewski@pgs-soft.com>
2018-04-09 13:08:18 +0200

    init commit

---
+++ LocalMessageReceiver/IEchoContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ServiceBus.Samples
+{
+    [ServiceContract(Name = "IEchoContract", Namespace = "http://samples.microsoft.com/ServiceModel/Relay/")]
+    public interface IEchoContract
+    {
+        [OperationContract]
+        string Echo(string text);
+    }
+
+    public interface IEchoChannel : IEchoContract, IClientChannel { }
+}
+
---
+++ LocalMessageReceiver/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Samples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalMessageReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBusEnvironment.SystemConnectivity.Mode = ConnectivityMode.AutoDetect;
+            string serviceNamespace = "servicebus0810";// Console.ReadLine();
+                                
+            string sasKey = " "; // namespace key
+
+            // Create the credentials object for the endpoint.
+            TransportClientEndpointBehavior sasCredential = new TransportClientEndpointBehavior();
+            sasCredential.TokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider("RootManageSharedAccessKey", sasKey);
+
+            // Create the service URI based on the service namespace.
+            Uri address = ServiceBusEnvironment.CreateServiceUri("sb", serviceNamespace, "EchoService");
+
+            // Create the service host reading the configuration.
+            ServiceHost host = new ServiceHost(typeof(EchoService), address);
+
+            // Create the ServiceRegistrySettings behavior for the endpoint.
+            IEndpointBehavior serviceRegistrySettings = new ServiceRegistrySettings(DiscoveryType.Public);
+
+            // Add the Relay credentials to all endpoints specified in configuration.
+            foreach (ServiceEndpoint endpoint in host.Description.Endpoints)
+            {
+                endpoint.Behaviors.Add(serviceRegistrySettings);
+                endpoint.Behaviors.Add(sasCredential);
+            }
+
+            // Open the service.
+            host.Open();
+            Console.WriteLine("Service address: " + address);
+            Console.WriteLine("Insert e to exit");
+            string input =null;
+            while (input != "e")
+            {
+                input = Console.ReadLine();
+            }
+
+            // Close the service.
+            host.Close();
+        }
+    }
+}
